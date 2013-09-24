4abfc0f8c049bd3703d6734a936cb0987df08beb
Hack Reactor Students <github@hackreactor.com>
2013-09-23 21:01:53 -0700

    Adds JSON.parse for everything except object

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,18 +4,23 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
   // your code goes here
-  //console.log(json);
-  console.log(typeof(json));
-  console.log(json);
   console.log("");
-  var jsonStr = stringifyJSON(json);
-  if (jsonStr.charAt(0) === '"'){
-    return jsonStr.slice()
-    // treat it as a string
-    //console.log(stringifyJSON(json.charAt(0)));
-    return stringifyJSON(json).charAt(0);
-  } else {
-    //console.log("not treated as a string");
+  console.log(json);
+  if (json === 'true'){
+    return true;
+  } else if (json === 'false'){
+    return false;
+  } else if (json === 'null'){
+    return null;
+  } else if (json.charAt(0) === '"'){
+    return json.slice(1,-1);
+  } else if (typeof(json) === 'number') {
     return json;
+  } else if (json.charAt(0) === '['){
+    return _.map(praseJSON(json));
+  } else if (json.charAt(0) === '{'){
+    for (var key in keys) {
+
+    }
   }
 };
